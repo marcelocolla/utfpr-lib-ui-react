aecdf0bcd884a1c651227c4e91cf0df1f19fd3ee
Marcelo Colla <marcelo.colla@pravaler.com.br>
2022-05-01 14:27:24 -0300

    add componente Header

---
+++ src/Header/Header.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useHistory } from 'react-router'
+
+import { Button } from '@ui/Button/Button'
+import { Modal } from '@ui/Modal'
+
+import { HeaderStyled, LogoutModalStyled } from './styles'
+
+type HeaderProps = {
+  header: string
+  home?: boolean
+  signOut: () => void
+}
+
+export const Header = ({ header, signOut, home: isHomePage }: HeaderProps) => {
+  const history = useHistory()
+  const [open, setOpen] = React.useState(false)
+
+  function handleClick() {
+    if (isHomePage) {
+      setOpen(true)
+    } else {
+      history.goBack()
+    }
+  }
+
+  return (
+    <>
+      <HeaderStyled>
+        <div onClick={handleClick}>
+          <img src="/icon/arrow-left.svg" alt="Icone de voltar" />
+        </div>
+        <strong>{header}</strong>
+      </HeaderStyled>
+
+      <Modal visible={open} close={() => setOpen(false)} title="Deseja sair?">
+        <LogoutModalStyled>
+          <Button name="logoutBtnYes" onClickFunction={signOut}>
+            Sim
+          </Button>
+          <Button name="logoutBtnNo" onClickFunction={() => setOpen(false)}>
+            Não
+          </Button>
+        </LogoutModalStyled>
+      </Modal>
+    </>
+  )
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 export * from './Button'
 export * from './Icons'
 export * from './Spinner'
+
+export * from './Header'
 export * from './Card'
 export * from './Modal'
+
 export * from './Form/FormSection/FormBody'
 export * from './Form/FormSection/FormFooter'
 export * from './Form/FormSection/FormLine'
