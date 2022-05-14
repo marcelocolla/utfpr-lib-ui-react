9823e7eb23282de4fde1e300756b439f91786ea9
Marcelo Colla <marcelo.colla@contasimples.com>
2022-05-13 22:03:26 -0300

    ajustando warning SVGs

--- src/Icons/ArrowLeftIcon/ArrowLeftIcon.tsx
+++ src/Icons/ArrowLeftIcon/ArrowLeftIcon.tsx
@@ -8,7 +8,7 @@ type ArrowLeftIconProps = {
 export const ArrowLeftIcon = ({ width = '8', height = '14' }: ArrowLeftIconProps) => {
   return (
     <svg width={width} height={height} viewBox="0 0 8 14" fill="none" xmlns="https://www.w3.org/2000/svg">
-      <path d="M7 13L1 7L7 1" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M7 13L1 7L7 1" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   )
 }
