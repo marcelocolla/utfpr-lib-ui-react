a2a26c968c03f4816e4943f26cd8c295cc7a812a
Marcelo Colla <marcelo.colla@contasimples.com>
2022-11-16 21:19:59 -0300

    adiciona exportação de resultados em CSV

--- metrics/plato-analyze.js
+++ metrics/plato-analyze.js
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const plato = require('plato-analyze')
 const pkg = require('../package.json')
+const reportToCsv = require('./reportToCsv')
 
 //be sure and set your src, output, and any options.
-const src = ['./dist/**/*.js']
+const src = ['./src/**/*.tsx', './src/**/*.ts']
 const outputDir = './metrics/artifacts'
 
 const platoArgs = {
@@ -28,6 +30,8 @@ function callback(reports) {
     maintainability: ${average.maintainability}`
 
   console.log(output)
+
+  reportToCsv(overview.reports)
 }
 
 // usage is plato.inspect
